84472caefad3ef83bfd99cdf8303ac074c32dcc5
ShlyaPn1kGit <rostislavdr2008@gmail.com>
2026-09-25 21:36:17 +0300

    Update prac_4.py

--- Tasks/Practice2/prac_4.py
+++ Tasks/Practice2/prac_4.py
@@ -1,46 +1,81 @@
 import multiprocessing as mp
+import time
 import sys
-sys.setrecursionlimit(1000000)
+
+sys.setrecursionlimit(999999999)
+
+array = [n for n in range(20000, 0, -1)]
+
+
+def timer(func):
+    def wrapper(*args, **kwargs):
+        start = time.perf_counter()
+        result = func(*args, **kwargs)
+        end = time.perf_counter()
+        print(f"Функция {func.__name__} выполнилась за {end - start:.6f} сек\n")
+        return result
+    return wrapper
+
 
 def divide(array, low, high):
     pivot = array[high]
     i = low - 1
+
     for j in range(low, high):
         if array[j] <= pivot:
             i += 1
             array[i], array[j] = array[j], array[i]
+
     array[i + 1], array[high] = array[high], array[i + 1]
     return i + 1
+
 
 def quick_sort(array, low, high):
     if low < high:
         pi = divide(array, low, high)
         quick_sort(array, low, pi - 1)
         quick_sort(array, pi + 1, high)
+
     return array
 
+
+@timer
 def parallel_quick_sort(array, low, high, num_threads, N=10000):
-    if low < high and high - low > N and num_threads > 1:
+    if low >= high:
+        return array
+    if high - low > N and num_threads > 1:
         pi = divide(array, low, high)
-        left, right = array[low:pi], array[pi + 1:high + 1]
-        
+
+        left = array[low:pi]
+        right = array[pi + 1:high + 1]
+
         with mp.Pool(processes=2) as pool:
-            left_res, right_res = pool.map(
-                lambda args: parallel_quick_sort(*args),
-                [(left, 0, len(left) - 1, num_threads // 2, N),
-                 (right, 0, len(right) - 1, num_threads // 2, N)]
+            left_res, right_res = pool.starmap(
+                quick_sort,
+                [
+                    (left, 0, len(left) - 1),
+                    (right, 0, len(right) - 1)
+                ]
             )
-        
-        array[low:pi], array[pi + 1:high + 1] = left_res, right_res
-    elif low < high:
-        pi = divide(array, low, high)
-        parallel_quick_sort(array, low, pi - 1, num_threads, N)
-        parallel_quick_sort(array, pi + 1, high, num_threads, N)
-    
+
+        array[low:pi] = left_res
+        array[pi + 1:high + 1] = right_res
+
+    else:
+        quick_sort(array, low, high)
+
     return array
 
-target = [i for i in range(20000, 0, -1)]
 
-#shell(target)
-quick_sort(target, 0, len(target)-1)
+if __name__ == "__main__":
+    mp.freeze_support()
 
+    result = parallel_quick_sort(
+        array,
+        0,
+        len(array) - 1,
+        8,
+        10000
+    )
+
+    print(result[:10])
